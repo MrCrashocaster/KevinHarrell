c8a1aaa7ce56ddb4608a1999ce3a17da6eca6ed9
MrCrashocaster <mrcrashocaster@gmail.com>
2022-02-12 22:26:08 -0600

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,41 @@
-$(window).resize(function() {
-    var desktop = document.getElementById("headline-2");
-    var mobile = document.getElementById("headline-2-mobile");
+$( document ).ready(function() {
 
-    desktop.classList.remove("animate__fadeIn");
-    mobile.classList.remove("animate__fadeIn");
+    if ($(window).width() <= 768) {
+        $('.target').addClass('display-none');
+        $('.buttonItem').addClass('btn');
+        $('.btn-hover').removeClass('btn-hover');
+    }
+
+    if ($(window).width() >= 768) {
+        $('.target').removeClass('display-none');
+        $('.buttonItem').removeClass('btn');
+        $('.btn-hover-2').addClass('btn-hover');
+    }
+    
+
+    $(window).resize(function() {
+        var desktop = document.getElementById("headline-2");
+        var mobile = document.getElementById("headline-2-mobile");
+    
+        if ($(window).width() <= 768) {    
+            $('.target').addClass('display-none');
+            $('.buttonItem').addClass('btn');
+            $("#control").css({ 'color' : '$darkGrey'});
+            $('.btn-hover').removeClass('btn-hover');
+        }
+
+        if ($(window).width() >= 768) {
+            $('.target').removeClass('display-none');
+            $('.buttonItem').removeClass('btn');
+            $('.btn-hover-2').addClass('btn-hover');
+        }
+    
+        desktop.classList.remove("animate__fadeIn");
+        mobile.classList.remove("animate__fadeIn");
+    });
 });
+
+
+
+
+
