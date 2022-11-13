1524ef6eb20526ed82008b513fbac2f64d4db844
MrCrashocaster <mrcrashocaster@gmail.com>
2022-11-13 00:59:53 -0600

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -33,7 +33,6 @@ $( document ).ready(function() {
 
     jQuery(function($) {
     var path = window.location.href; 
-    // because the 'href' property of the DOM element is the absolute path
     $('#quickSelectionLinks a').each(function() {
         if (this.href === path) {
         $(this).addClass('deactivated');
