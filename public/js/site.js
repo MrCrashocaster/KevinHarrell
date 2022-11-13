d65c1e071299ecd555c1f5213bf0913e34816db0
MrCrashocaster <mrcrashocaster@gmail.com>
2022-11-12 20:13:27 -0600

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 
+    // $('.titles').text("Ever since middle school in my small town of less than 1,200 people, I was the computer guy. In the age of Napster, being the computer guy came with its advantages. Students would give me a list of their favorite songs, I would, in turn, go to Napster —> Find their favorite songs —> Burn the CD —> Make $15. Repeat. Fast forward to high school, I was always up-to-date with the latest Nokia, Motorola or Kyocera cellphone.");
+
     jQuery(function($) {
     var path = window.location.href; 
     // because the 'href' property of the DOM element is the absolute path
