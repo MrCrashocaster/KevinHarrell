aafce1172c48dbb0f1888f983efd0109c8a3d385
MrCrashocaster <mrcrashocaster@gmail.com>
2022-11-12 23:56:08 -0600

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -2,6 +2,35 @@ $( document ).ready(function() {
 
     // $('.titles').text("Ever since middle school in my small town of less than 1,200 people, I was the computer guy. In the age of Napster, being the computer guy came with its advantages. Students would give me a list of their favorite songs, I would, in turn, go to Napster —> Find their favorite songs —> Burn the CD —> Make $15. Repeat. Fast forward to high school, I was always up-to-date with the latest Nokia, Motorola or Kyocera cellphone.");
 
+
+    $('#projectSelection').on('change', function(){
+    	var selectValue = $(this).val();
+        console.log(selectValue);
+        $('#hobby').removeClass('hide');
+        $('#freelance').removeClass('hide');
+        $('#hobby').parent().removeClass('hide');
+        $('#freelance').parent().removeClass('hide');
+        $("#alert").removeClass('showAlert');
+        if (selectValue == 'hobby') {
+            $('#hobby').addClass('show-flex');
+            $('#freelance').addClass('hide');
+            $('#freelance').parent().addClass('hide');
+        }
+        else if (selectValue == 'freelance') {
+            $('#freelance').addClass('show-flex');
+            $('#hobby').addClass('hide');
+            $('#hobby').parent().addClass('hide');
+        }
+        else if (selectValue == 'all') {
+            $('#hobby').addClass('show-flex');
+            $('#freelance').addClass('show-flex');
+        }
+        else {
+            $("#projectSelection").val('all');
+            $("#alert").addClass('showAlert');
+        }
+    });
+
     jQuery(function($) {
     var path = window.location.href; 
     // because the 'href' property of the DOM element is the absolute path
@@ -10,6 +39,7 @@ $( document ).ready(function() {
         $(this).addClass('deactivated');
         $(this).children('#control').addClass('lightGrey');
         $(this).children('.prompt').addClass('lightGrey');
+        $(this).children('#control').children('.itemTag').children('.projectTag').css('color', '#848181');
         }
     });
     });
